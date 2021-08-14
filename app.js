37bebc09c7d731a27f0659b7373695c683f16c97
maria acuna <taeny90@gmail.com>
2021-08-14 13:41:24 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+async function searchGIF() {
+    let config = {
+        params: {
+            tag: `${$('#search')[0].value}`,
+            api_key: "6qahPjYEe2wyuH4XR1tIe8864MhnLuhJ"
+        }
+    }
+    
+    console.log($('#search'));
+    let response = await axios.get('http://api.giphy.com/v1/gifs/random', config);
+    renderGIFs(response.data.data.image_url)
+}
+
+function renderGIFs(GIF) {
+    $('#container').append(`<img src=${GIF}>`)
+}
+
+$('#search-gif').on('click', e => {
+    e.preventDefault();
+    searchGIF();
+});
+
+$('#remove-gif').on('click', e => {
+    e.preventDefault();
+    $('#container').empty();
+});
